a7445976c2b8d79be40d6c3fb39b16015e21a9e3
anzisheng <anzisheng@163.com>
2025-02-07 18:36:40 +0800

    add mutex

--- blackboard_fighting/thread_sample1.cpp
+++ blackboard_fighting/thread_sample1.cpp
@@ -1,6 +1,6 @@
 #include <thread>
 #include <iostream>
-
+#include <mutex>
 using namespace std;
 
 void PeterWrite(std::string &blackboard)
@@ -23,17 +23,41 @@ void DannyWrite(std::string &blackboard)
     this_thread::sleep_for(std::chrono::milliseconds(rand()%3));  
     blackboard += "Danny \n";    
 }
+void PeterWriteWithMutex(mutex& amutex, std::string &blackboard)
+{
+    amutex.lock();
+    PeterWrite(blackboard);
+    amutex.unlock();
+}
+void DannyWriteWithMutex(mutex& amutex, std::string &blackboard)
+{
+    amutex.lock();
+    DannyWrite(blackboard);
+    amutex.unlock();
+
+}
+
+void TestNormalSafeLock(){
+    std::string blockboard;
+    std::mutex amutex;
+    thread DannyThread(DannyWriteWithMutex, std::ref(amutex), std::ref(blockboard));
+    thread PeterThread(PeterWriteWithMutex, std::ref(amutex), std::ref(blockboard));
+    DannyThread.join();
+    PeterThread.join();
+    std::cout << blockboard << std::endl;
+}
 
 
 void DemoResourceeConflict(){
-    std::string blackboard;
-    thread DannyThread(DannyWrite, std::ref(blackboard));
-    thread PeterThread(PeterWrite, std::ref(blackboard));
-    DannyThread.join();
-    PeterThread.join();
-    std::cout << blackboard <<std::endl; 
+    // std::string blackboard;
+    // thread DannyThread(DannyWrite, std::ref(blackboard));
+    // thread PeterThread(PeterWrite, std::ref(blackboard));
+    // DannyThread.join();
+    // PeterThread.join();
+    // std::cout << blackboard <<std::endl; 
 }
 int main(){
-    DemoResourceeConflict();
+    //DemoResourceeConflict();
+    TestNormalSafeLock();
     return 0;
 }
